1c7fdf590beb78649e120422f8bbb5e5d368e66c
k-k86 <143694521+k-k86@users.noreply.github.com>
2023-09-01 17:03:58 +0900

    Update q4.php

--- クラス化前PHP/q4.php
+++ クラス化前PHP/q4.php
@@ -1,8 +1,8 @@
 <?php 
     //DBに接続する為の処理
     $dsn = 'mysql:dbname=kadais;host=localhost;charset=utf8mb4';
-    $user = 'root';
-    $password = 'ozi123';
+    $user = 'ユーザ名';
+    $password = 'パスワード';
     try { 
         $dbh = new PDO($dsn, $user, $password);     
         } catch(PDOException $e) {
